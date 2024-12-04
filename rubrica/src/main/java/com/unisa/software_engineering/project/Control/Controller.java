188d8d6c609bb7b1a7e004b186b3a66ecea1bde8
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2024-12-04 17:08:00 +0100

    add new classes

--- rubrica/src/main/java/com/unisa/software_engineering/project/Control/Controller.java
+++ rubrica/src/main/java/com/unisa/software_engineering/project/Control/Controller.java
@@ -3,15 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.unisa.software_engineering.project.Model;
+package com.unisa.software_engineering.project.Control;
 
-import java.util.List;
 
 /**
  *
  * @author paolo
  */
-public class Rubrica {
+public class Controller {
     
-    private List<Contatto> contatti;
+    
+    
 }
--- rubrica/src/main/java/com/unisa/software_engineering/project/Model/Contatto.java
+++ rubrica/src/main/java/com/unisa/software_engineering/project/Model/Contatto.java
@@ -9,6 +9,74 @@ package com.unisa.software_engineering.project.Model;
  *
  * @author paolo
  */
-public class Contatto {
+public class Contatto implements Comparable<Contatto> {
     
+    private String nome;
+    private String cognome;
+    private String[] numeriDiTelefono;
+    private String[] emails;
+    private ImmagineProfilo immagineProfilo;
+
+    public Contatto(String nome, String cognome, String[] numeriDiTelefono, String[] emails, ImmagineProfilo immagineProfilo) {
+        
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numeriDiTelefono = numeriDiTelefono;
+        this.emails = emails;
+        this.immagineProfilo = immagineProfilo;
+    }
+
+    public String getNome() {
+        
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        
+        this.cognome = cognome;
+    }
+
+    public String[] getNumeriDiTelefono() {
+        
+        return numeriDiTelefono;
+    }
+
+    public void setNumeriDiTelefono(String[] numeriDiTelefono) {
+        
+        this.numeriDiTelefono = numeriDiTelefono;
+    }
+
+    public String[] getEmails() {
+        
+        return emails;
+    }
+
+    public void setEmails(String[] emails) {
+        
+        this.emails = emails;
+    }
+
+    public ImmagineProfilo getImmagineProfilo() {
+        return immagineProfilo;
+    }
+
+    public void setImmagineProfilo(ImmagineProfilo immagineProfilo) {
+        this.immagineProfilo = immagineProfilo;
+    }
+    
+    @Override
+    public int compareTo(Contatto c) {
+        
+        return this.nome.compareTo(c.nome);
+    }
 }
