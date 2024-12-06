dc5ebc426d2db0ff713ae4ecc7a7a9002efa3332
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2024-12-06 10:07:08 +0100

    add Exceptions

---
+++ rubrica/src/main/java/com/unisa/software_engineering/project/Exceptions/NomeNonValidoException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.unisa.software_engineering.project.Exceptions;
+
+/**
+ *
+ * @author andre
+ */
+public class NomeNonValidoException extends RubricaExceptions{
+    
+    public NomeNonValidoException(String msg) {
+        super(msg);
+    }
+    
+}
