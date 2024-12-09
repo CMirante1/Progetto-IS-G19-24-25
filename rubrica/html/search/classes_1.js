57e519ff1d9731a071ff82fb64cd8a69f01bb990
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2024-12-09 18:12:18 +0100

    update

--- rubrica/html/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['emailnonvalidaexception_0',['EmailNonValidaException',['../classcom_1_1unisa_1_1software__engineering_1_1project_1_1_exceptions_1_1_email_non_valida_exception.html',1,'com::unisa::software_engineering::project::Exceptions']]]
-];
--- rubrica/html/search/classes_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['rubrica_0',['Rubrica',['../classcom_1_1unisa_1_1software__engineering_1_1project_1_1_model_1_1_rubrica.html',1,'com::unisa::software_engineering::project::Model']]],
-  ['rubricaapp_1',['RubricaApp',['../classcom_1_1unisa_1_1software__engineering_1_1project_1_1_rubrica_app_1_1_rubrica_app.html',1,'com::unisa::software_engineering::project::RubricaApp']]]
-];
