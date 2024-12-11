59f96e4b53eb0f228e16f3a142b5311763cc53b4
CMirante1 <c.mirante1@studenti.unisa.it>
2024-12-11 18:10:51 +0100

    Fixato verifica Email

--- rubrica/src/main/java/com/unisa/software_engineering/project/model/Email.java
+++ rubrica/src/main/java/com/unisa/software_engineering/project/model/Email.java
@@ -10,10 +10,27 @@ public class Email extends InfoContatto implements Serializable{
     
     @Override
     public void verifica(String info) throws InfoContattoException{
-        if (!(info == null ||
-            info.trim().isEmpty() ||
-            !info.matches(
-            "^[\\\\w!#$%&’*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$")))
-            throw new InfoContattoException("Email non valida");
+
+        if (info == null)
+            throw new InfoContattoException("Email null");
+
+        if (info.isEmpty())
+            throw new InfoContattoException("Email vuota");
+
+        if (!info.contains("@"))
+            throw new InfoContattoException("Email senza @");
+
+        if (info.split("@").length != 2)
+            throw new InfoContattoException("Formato email non valido");
+
+        if (!info.split("@")[0]
+            .matches("^[a-zA-Z0-9._+&*-]+([a-zA-Z0-9._+&*-]*[a-zA-Z0-9])?$"))
+            throw new InfoContattoException("Email local part non valida");
+
+        if (!info.split("@")[1]
+            .matches("^[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,}$"))
+            throw new InfoContattoException("Email dominio non valido");
+        //if(!info.matches("^[\\\\w!#$%&’*+/=?{|}~^-]+(?:\\\\.[\\\\w!#$%&’*+/=?{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$"))
+        //    throw new InfoContattoException("Email dominio non valido");
     }
 }
